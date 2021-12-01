69d31e91af21a7f3a3d50a427cf42e8ca27c9579
Roberto Alfredo Meza Meza <mezamezar87@univie.ac.at>
2021-12-01 02:25:37 +0100

    Finished FileReader
    Finished implementing FileReader and added directory and files for the first day.

--- src/FileReader.h
+++ src/FileReader.h
@@ -1,25 +1,43 @@
 /*
   This class reads and provide the data from the files which are given by
-  the organizers. 
+  the organizers.
 */
 
 #include <iostream>
+#include <exception>
 #include <fstream>
 #include <vector>
+#include <string>
 
-class FileReader {
-    private:
-        std::string line;
-        std::vector< std::string > file_input;
+class FileReader
+{
+private:
+    std::vector<std::string> input_data;
 
-        std::ifstream input_file;
+    std::ifstream input_file;
 
-    public:
-
-        FileReader(std::string file) {
-            try {
-                this->input_file.open(file); 
-            }
-            
+public:
+    FileReader(std::string file)
+    {
+        try
+        {
+            this->input_file.open(file);
         }
+        catch (const std::exception &e)
+        {
+            std::cerr << "An error occurred: " << e.what() << "\n";
+        }
+    }
+
+    std::vector<std::string> getStringData()
+    {
+        std::string line;
+
+        while (std::getline(this->input_file, line))
+        {
+            input_data.push_back(line);
+        }
+
+        return this->input_data;
+    }
 };
