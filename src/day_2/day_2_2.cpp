fe2c36700bf7b7edaba76a370f7fb0efb5123e4b
Roberto Alfredo Meza Meza <mezamezar87@univie.ac.at>
2021-12-02 16:59:03 +0100

    Day 2 - Part 1 and Part 2

---
+++ src/day_2/day_2_2.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <algorithm>
+
+#include "../filereader.h"
+
+int main() {
+    FileReader fileReader{ "input.txt" };
+    std::vector<std::string> str_lines{fileReader.getStringData()};
+
+    std::vector<std::string> directions;
+    std::vector<int> direction_values;
+
+    int forward{ 0 };
+    int depth{ 0 };
+    int aim{ 0 };
+
+    std::for_each(str_lines.begin(), str_lines.end(),
+        [&](std::string line) {
+
+            std::size_t pos{ line.find(" ") };
+            std::size_t length{ pos - 0 };
+
+            directions.push_back(line.substr(0, length));
+            direction_values.push_back(std::stoi(line.substr(pos)));
+        });
+
+
+    for(int i{ 0 }; i < directions.size(); i++) {
+        if(directions.at(i) == "forward") {
+            forward += direction_values.at(i);
+            depth += aim * direction_values.at(i);
+        }
+        else if(directions.at(i) == "down") {
+            aim += direction_values.at(i);
+        }
+        else {
+            aim -= direction_values.at(i);
+        }
+    }
+
+    std::cout << "Muliply final depth position by final horizontal position: " << forward * depth << "\n";
+
+    return 0;
+}
